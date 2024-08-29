ae25a1a13976380e637afef23f4c1365846a1609
David Rivero <davidmiguel22573@gmail.com>
2024-08-29 03:44:56 -0700

    add outlines for quizes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from app.utils.helpers import ask_ai
 
 
+with open("./data/outline.txt", "r") as f:
+    outline = f.read()
+
+
+def generate_quiz(book, chapter):
+    return ask_ai(
+        f"Generate a quiz for {book} {chapter} that follows this outline: "
+        + outline
+        + "?"
+    )
+
+
 def main():
-    ask_ai("Monte Carlo first year?")
+    generate_quiz("Genesis", 1)
+    pass
 
 
 if __name__ == "__main__":
