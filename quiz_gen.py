a0b73d6e629b95e05944eaa143fd8e7f4aaad15e
David Rivero <davidmiguel22573@gmail.com>
2024-09-02 16:22:39 -0700

    Turn quiz generation into a class

--- quiz_gen.py
+++ quiz_gen.py
@@ -1,67 +1,42 @@
-import os
 import time
 from app.utils.helpers import ask_ai
 import json
 
 
-with open("data/quiz-template.json") as f:
-    outline = json.load(f)
+class QuizGenerator:
+    def __init__(self, book, chapter):
+        self.book = book
+        self.chapter = chapter
 
+        with open("data/quiz-template.json") as f:
+            self.outline = json.load(f)
 
-def write_instructions(path, book, chapter):
-    instruction = (
-        "You are an important JSON generator specializing in Biblical content. "
-        "You are extremely careful and meticulous. "
-        f"Create a comprehensive JSON structure for {book} chapter {chapter} based on the provided template. "
-        "Follow these guidelines meticulously:\n"
-        "1. Populate all fields with accurate and relevant information from the specified chapter.\n"
-        "2. Generate the exact number of questions for each section as specified in the 'quiz_structure':\n"
-        "   - 'Factual Recall': Exactly 10 multiple-choice questions\n"
-        "   - 'Comprehension and Analysis': Exactly 6 multiple-choice questions\n"
-        "   - 'Application': Exactly 2 open-ended questions\n"
-        "   - 'Reflection': Exactly 2 open-ended questions\n"
-        "3. Ensure each question strictly adheres to its designated type and section.\n"
-        "4. For multiple-choice questions, "
-        "always provide exactly four options and indicate the correct answer index (0-3).\n"
-        "5. For open-ended questions, use the 'input' type for options and provide a model answer.\n"
-        "6. Fill in all other sections ('key_themes', 'key_verses', 'characters', 'events', 'concepts', "
-        "'cross_references') with relevant content. Do not omit any sections.\n"
-        "7. Double-check for accuracy, completeness, and strict adherence to the template structure.\n"
-        "8. Use double quotes for property names.\n"
-        "9. Ensure the output is valid JSON and UTF-8 encoded.\n"
-        "10. Ensure all arrays and objects are properly closed with matching brackets and braces.\n"
-        "11. Double-check that all required fields are present and no extra fields are added.\n"
-        "12. Verify that all numerical values are not enclosed in quotes.\n"
-        "13. Ensure that boolean values are lowercase (true or false) and not enclosed in quotes.\n"
-        "14. Provide only the resulting JSON in your response, without any additional text, explanations.\n"
-        "15. The response should start with '{' and end with '}' without any other characters before or after.\n\n"
-        f"Here's the template to follow, follow it exactly no deviatons and is valid:\n{outline}"
-    )
+    def get_instructions(self):
+        with open("./data/prompt.txt") as f:
+            instructions = f.read()
+        instructions = instructions.replace("{book}", self.book)
+        instructions = instructions.replace("{chapter}", str(self.chapter))
+        return instructions
 
-    with open(path, "w+", encoding="utf-8") as f:
-        f.write(instruction)
+    def generate(self):
+        prompt_path = "./data/prompt.txt"
+        data = ask_ai(prompt_path)
+        file_path = f"data/{self.book}/{self.book.lower()}-{self.chapter}.json"
+        self.write_json(file_path, data)
 
-
-def generate_quiz(book, chapter):
-    prompt_path = "./data/prompt.txt"
-    write_instructions(prompt_path, book, chapter)
-    data = ask_ai(prompt_path)
-    file_path = f"data/{book}/{book.lower()}-{chapter}.json"
-    write_json(file_path, data)
-
-
-def write_json(path, data):
-    with open(path, "w+", encoding="utf-8") as f:
-        data = json.loads(data, strict=False)
-        json.dump(data, f, indent=4)
+    def write_json(self, path, data):
+        with open(path, "w+", encoding="utf-8") as f:
+            data = json.loads(data, strict=False)
+            json.dump(data, f, indent=4)
 
 
 def generate_book(book, ch_nums):
     chapters = [i for i in range(1, ch_nums + 1)]
     for chapter in chapters:
         try:
+            qzgen = QuizGenerator(book, chapter)
             print(f"Generating {book} chapter {chapter}")
-            generate_quiz(book, chapter)
+            qzgen.generate()
             if chapter != chapters[-1]:
                 time.sleep(60 * 1)
             print(f"Finished {book} chapter {chapter}")
@@ -71,6 +46,13 @@ def generate_book(book, ch_nums):
             pass
 
 
+def generate_chapter(book, chapter):
+    qzgen = QuizGenerator(book, chapter)
+    print(f"Generating {book} chapter {chapter}")
+    qzgen.generate()
+    print(f"Finished {book} chapter {chapter}")
+
+
 def generate_missing_chapters():
     with open("./data/missing_chapters.txt") as f:
         missing_chapters = f.readlines()
@@ -78,19 +60,15 @@ def generate_missing_chapters():
     for chapter in missing_chapters:
         chapter = chapter.strip()[:-5]
         book, chapter = chapter.split("-")
-        generate_quiz(book.capitalize(), chapter)
+        qzgen = QuizGenerator(book.capitalize(), chapter)
+        print(f"Generating {book} chapter {chapter}")
+        qzgen.generate()
+        print(f"Finished {book} chapter {chapter}")
 
 
 def main():
-    for file in os.listdir("./data/Genesis"):
-        with open(f"./data/Genesis/{file}") as q:
-            wc = len(q.readlines())
-            if wc > 400:
-                print(wc)
-                print(q.name)
+    pass
 
 
 if __name__ == "__main__":
-    # generate_book("Leviticus", 27)
-    # generate_missing_chapters()
-    pass
+    main()
